1cf7c3e3728792ce3f2e365d3be7064ba703d1f4
ccbien <chibien408@gmail.com>
2022-12-13 14:00:09 +0700

    add detect method

--- ocr/api.py
+++ ocr/api.py
@@ -1,7 +1,7 @@
 import yaml
 from argparse import ArgumentParser, Namespace
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from icecream import ic, install
 install()
 
@@ -10,21 +10,13 @@ from utils import get_image_from_url
 
 
 app = Flask(__name__)
-@app.route('/test')
-def test():
-    try:
-        global model, opt
-        opt.img = get_image_from_url('https://www.robots.ox.ac.uk/~vgg/software/textspot/text.png')
-        # opt.imshow = True
-        # opt.print_result = True
-        res = model.readtext(**vars(opt))
-        words = res[0]['text']
-        print('Detected words:', end=' ')
-        print(*words, sep=', ')
-        return jsonify({'words': words})
-
-    except Exception as e:
-        return jsonify('System Error')
+@app.route('/detect')
+def detect():
+    global model, opt
+    url = request.args.get('url')
+    opt.img = get_image_from_url(url)
+    res = model.readtext(**vars(opt))[0]['result']
+    return jsonify(res)
 
 
 if __name__ == '__main__':
@@ -32,10 +24,14 @@ if __name__ == '__main__':
     parser.add_argument('--option', '-o', type=str, default='options/default.yml',
         help='Option file')
     args = parser.parse_args()
+
     opt = Namespace(**yaml.safe_load(open(args.option, 'r')))
+    opt.details = True
+    opt.imshow = False
+    opt.print_result = False
     model = Model(
-        det='PANet_IC15',
-        recog='SEG',
+        det=opt.model_det,
+        recog=opt.model_recog,
         config_dir='configs/',
     )
     app.run()
